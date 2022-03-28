3224adf5f06ab566a595815abedca03bd8207b4d
vijaykumarh <amrvijayhiremath@gmail.com>
2022-03-28 17:38:40 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { isNgTemplate } from '@angular/compiler';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ToDo-App';
+  list:any[]=[];
+
+  addTask(item:string)
+  {
+    this.list.push({id:this.list.length,name:item})
+    console.warn(this.list);
+
+  }
+  removeTask(id:number)
+  {
+    console.warn(id)
+    this.list=this.list.filter(item=>item.id!==id);
+  }
+}
